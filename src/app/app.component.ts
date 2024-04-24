25449961cafb9caa0c767dac56a58e7fef07a3b1
JBaines98 <jackabaines@googlemail.com>
2024-04-24 16:42:52 +0100

    Event emitter for contactEntered

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Contact } from './contact.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ContactList';
+
+
+  //below should input parameter should be of type contact.
+    contactEntered(contact: any){}
 }
--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { ContactService } from '../contact.service';
 import { FormControl, FormGroup } from '@angular/forms';
+import { Contact } from '../contact.model';
+import { Subject, takeUntil, tap } from 'rxjs';
 
 @Component({
   selector: 'app-form',
@@ -10,8 +12,11 @@ import { FormControl, FormGroup } from '@angular/forms';
 export class FormComponent {
 
   // public contact: Contact = {};
+  public destroyed$ = new Subject();
 
   constructor(public contactService: ContactService){}
+
+  @Output() contactEntered = new EventEmitter();
 
   reactiveForm = new FormGroup({
     firstName: new FormControl(''),
@@ -25,5 +30,14 @@ export class FormComponent {
     dateOfBirth: new FormControl(''),
     telephone: new FormControl(0),
   })
+
+  ngOnInit(): void{
+    this.reactiveForm.valueChanges.pipe(
+      tap((changes) => {
+        this.contactEntered.emit(changes);
+      }),
+      takeUntil(this.destroyed$)
+    ).subscribe();
+  }
 
 }
