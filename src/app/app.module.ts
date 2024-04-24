954089a82dd5c86a75578ced643fc948d64cc847
JBaines98 <jackabaines@googlemail.com>
2024-04-24 13:27:02 +0100

    Adding basic components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { ContactListComponent } from './contact-list/contact-list.component';
+import { FormComponent } from './form/form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ContactListComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule
