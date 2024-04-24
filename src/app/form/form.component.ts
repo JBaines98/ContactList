60817a8359fa358f96406e3a7a554666599a9e35
JBaines98 <jackabaines@googlemail.com>
2024-04-24 14:20:48 +0100

    form setup

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ContactService } from '../contact.service';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
@@ -6,5 +8,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./form.component.css']
 })
 export class FormComponent {
+
+  // public contact: Contact = {};
+
+  constructor(public contactService: ContactService){}
+
+  reactiveForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    addressFirstLine: new FormControl(''),
+    addressSecondLine: new FormControl(''),
+    cityOrTown: new FormControl(''),
+    county: new FormControl(''),
+    dateOfBirth: new FormControl(''),
+    telephone: new FormControl(0),
+  })
 
 }
