f0c7c4317053d9d5eb10e54ada360acfc06ef475
JBaines98 <jackabaines@googlemail.com>
2024-04-30 12:03:10 +0100

    save contactList array to localStorage, and get back the string

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Contact } from './contact.model';
 import { ContactService } from './contact.service';
 import { Subject, takeUntil, tap } from 'rxjs';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -11,6 +12,7 @@ import { Subject, takeUntil, tap } from 'rxjs';
 export class AppComponent {
   title = 'ContactList';
   contactList: Contact[] = [];
+  contactListString: any = null;
   public destroyed$ = new Subject();
 
   constructor(public contactService: ContactService){}
@@ -22,11 +24,19 @@ export class AppComponent {
       }),
       takeUntil(this.destroyed$)
     ).subscribe();
+    let savedContacts = localStorage.getItem('app.contactList');
+    this.contactListString = savedContacts;
+    console.log(this.contactListString);
   }
 
   ngOnDestroy(): void {
     this.destroyed$.next(this.destroyed$);
     this.destroyed$.complete();
+  }
+
+  saveContactList(){
+    let contactListToSave = this.contactList;
+    localStorage.setItem('app.contactList', JSON.stringify(contactListToSave));
   }
 
   //below should input parameter should be of type contact.
